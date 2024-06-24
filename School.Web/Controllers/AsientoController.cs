068fe7b364c02ca183bafd473e358c6dcdbca2e5
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2024-06-24 19:42:46 -0400

    modulo de adm. de asientos completados.

--- School.Web/Controllers/AsientoController.cs
+++ School.Web/Controllers/AsientoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using School.Data.Entities;
 using School.Data.Interfaces;
 
 namespace School.Web.Controllers
@@ -23,7 +24,8 @@ namespace School.Web.Controllers
         // GET: AsientoController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var asiento = this.asientoRepository.ObtenerPorId(id);
+            return View(asiento);
         }
 
         // GET: AsientoController/Create
@@ -35,10 +37,11 @@ namespace School.Web.Controllers
         // POST: AsientoController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Asiento asiento)
         {
             try
             {
+                this.asientoRepository.Agregar(asiento);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -50,16 +53,24 @@ namespace School.Web.Controllers
         // GET: AsientoController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            {
+                var asiento = this.asientoRepository.ObtenerPorId(id);
+                return View(asiento);
+            }
+
+           
+
+
         }
 
         // POST: AsientoController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Asiento asiento)
         {
             try
             {
+                this.asientoRepository.Actualizar(asiento);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -68,25 +79,5 @@ namespace School.Web.Controllers
             }
         }
 
-        // GET: AsientoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: AsientoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
